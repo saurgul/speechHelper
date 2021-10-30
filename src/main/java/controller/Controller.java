faed34fc3089931485d1e174de8240d349c2339e
Christian Dummer <cjdummer@iu.edu>
2021-10-29 21:32:12 -0400

    Created controller with method to create speechtotext command

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.io.File;
+
+import command.Command;
+import model.Model;
+import speechtotext.SpeechToTextCommand;
+
+public class Controller {
+	private Model model;
+	
+	public Controller() {
+		
+	}
+	
+	public Controller(Model m) {
+		this.model = m;
+	}
+	
+	//Performs speech to text command
+	public void speechToText(File f) {
+		Command speechToTextCommand = new SpeechToTextCommand(model);
+	}
+}
