c17648921005bdb9c808c1c6cf1bd23c7a35190f
Christian Dummer <cjdummer@iu.edu>
2021-10-29 21:59:47 -0400

    Command invoker

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -3,15 +3,22 @@ package model;
 
 import java.util.ArrayList;
 
+import command.Command;
+import command.CommandInvoker;
+
 //Basic model with an arraylist of speeches. Will need to move type from string once we have longer speeches but string will do for now.
 public class Model {
 	
 	private ArrayList<String> speeches;
+	private CommandInvoker commandInvoker;
 	
 	public Model() {
 		speeches = new ArrayList<String>();
+		this.commandInvoker = new CommandInvoker();
 	}
 	
+	//TODO use a database for this 
+	//Add, remove, and get list of speech transcriptions
 	public ArrayList<String> getSpeeches(){
 		return this.speeches;
 	}
@@ -22,6 +29,12 @@ public class Model {
 	
 	public void removeSpeech(String s) {
 		speeches.remove(s);
+	}
+	
+	
+	//Use commandInvoker
+	public void receiveCommand(Command c) {
+		commandInvoker.receiveCommand(c);
 	}
 
 }
