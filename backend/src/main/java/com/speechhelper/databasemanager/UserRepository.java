7fe2148f57de16e6ab6ddb0d00b1894ce8082698
Christian Dummer <cjdummer@iu.edu>
2021-11-12 16:12:29 -0500

    Added methods to userrepository

--- backend/src/main/java/com/speechhelper/databasemanager/UserRepository.java
+++ backend/src/main/java/com/speechhelper/databasemanager/UserRepository.java
@@ -2,10 +2,13 @@ package com.speechhelper.databasemanager;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 @Repository
-public interface UserRepository extends CrudRepository <UserEntity, Long> {
-	
+public interface UserRepository extends JpaRepository <UserEntity, Long> {
+	List<UserEntity> findAll();
+	UserEntity findById(long id);
 }
