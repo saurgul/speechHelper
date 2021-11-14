33b1a3bbef91b16b0529da3514d390d6dbea558c
Christian Dummer <cjdummer@iu.edu>
2021-11-14 16:57:35 -0500

    Created command for generating report

--- backend/src/main/java/com/speechhelper/speechtotext/GenerateReportCommand.java
+++ backend/src/main/java/com/speechhelper/speechtotext/GenerateReportCommand.java
@@ -14,6 +14,7 @@ public class GenerateReportCommand implements Command {
 
 	private Model model;
 	private Speech speech;
+	private SpeechToTextReport report;
 	
 	public GenerateReportCommand(Model m, Speech s) {
 		this.model = m;
@@ -21,7 +22,36 @@ public class GenerateReportCommand implements Command {
 	}
 	
 	public void execute() { 
+		report = speech.getReport();
+		wordFrequencyFeedback();
+		fillerFrequencyFeedback();
+		fillerRatioFeedback();
+		speechRateFeedback();
+	}
+	
+	private void wordFrequencyFeedback() {
+		//TODO frequency feedback
+	}
+	
+	private void fillerFrequencyFeedback() {
+		//TODO fillerFrequencyFeedback
+	}
+	
+	private void fillerRatioFeedback() {
+		//TODO fillerRatioFeedback
+	}
+	
+	private void speechRateFeedback() {
+		double rate = report.getSpeechRate();
+		String feedback = "";
+		if(rate < 140) {
+			feedback = "Your speaking is a little on the slower end. Try picking up the pace! An average speaking rate is ~140 - 160 words per minute.";
+		}
+		else if(rate > 160) {
+			
+		}
 		
+		report.setFeedback(report.getFeedback() + feedback);
 	}
 
 	public void unexecute() {
--- backend/src/main/java/com/speechhelper/speechtotext/SpeechToTextReport.java
+++ backend/src/main/java/com/speechhelper/speechtotext/SpeechToTextReport.java
@@ -23,6 +23,48 @@ public class SpeechToTextReport {
 		this.feedback = sb.feedback;
 	}
 	
+	//Getter methods
+	public String getWordFrequency() {
+		return this.wordFrequency;
+	}
+	
+	public String getFillerFrequency() {
+		return this.fillerFrequency;
+	}
+	
+	public String getFillerRatio() {
+		return this.fillerRatio;
+	}
+	
+	public double getSpeechRate() {
+		return this.speechRate;
+	}
+	
+	public String getFeedback() {
+		return this.feedback;
+	}
+	
+	//Setter methods
+	public void setWordFrequency(String s) {
+		this.wordFrequency = s;
+	}
+	
+	public void setFillerFrequency(String s) {
+		this.fillerFrequency = s;
+	}
+	
+	public void setFillerRatio(String s) {
+		this.fillerRatio = s;
+	}
+	
+	public void setSpeechRate(double d) {
+		this.speechRate = d;
+	}
+	
+	public void setFeedback(String s) {
+		this.feedback = s;
+	}
+	
 	
 	//Implementation of builder pattern
 	public static class Builder{
