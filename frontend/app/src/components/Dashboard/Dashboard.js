fd9c748c0970f4427869a90fb46f84886326bdc0
Saurabh Gulati <waitforit@Saurabhs-MacBook-Air.local>
2021-12-11 21:52:01 -0500

    Updated Audio Files for Famous Speeches

--- frontend/app/src/components/Dashboard/Dashboard.js
+++ frontend/app/src/components/Dashboard/Dashboard.js
@@ -14,10 +14,15 @@ function Dashboard(){
 	const [showHelp, setShow] = useState(false);
 	const [showProgress, setProgress] = useState(false);
 	const [speechText, setSpeech] = useState("I have a dream that one day down in Alabama, with its vicious racists, with its governor having his lips dripping with the words of interposition and nullification – one day right there in Alabama little black boys and black girls will be able to join hands with little white boys and white girls as sisters and brothers. I have a dream today. I have a dream that one day every valley shall be exalted and every hill and mountain shall be made low, the rough places will be made plain, and the crooked places will be made straight, and the glory of the Lord shall be revealed and all flesh shall see it together.") 
+	const [audioPath, setAudioPath] = useState("./Assets/Famous Speeches/") ;
 	const [showRecordLiveModal, setRecordLiveModal] = useState(false);
 
 	const updateSpeech = (newSpeech) => {
 		setSpeech(newSpeech)
+	}
+
+	const updateAudioPath = (newAudioPath) => {
+		setAudioPath(newAudioPath)
 	}
 
 	const data = [
